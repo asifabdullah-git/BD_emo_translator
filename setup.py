8155a799f139c27bc391bbcf056cad7ecb046013
asif abdullah <asifabdullah6@gmail.com>
2020-06-08 17:14:02 +0600

    setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+"""
+Setup script for emoji
+"""
+from setuptools import setup
+
+setup(
+    name='BD_emo_translator',
+    version="1.0",
+    author='Abdullah Al Asif',
+    author_email='asifabdullah6@gmail.com',
+    description="Emoji and Emoticons detection and convert into Bangla package for Python",
+    keywords=['emoji, emoticons'],
+    include_package_data=True,
+    license="MIT License",
+    packages=['BD_emo_translator'],
+    url="https://github.com/asifabdullah-git/BD_emo_translator",
+    zip_safe=True,
+    download_url="https://github.com/asifabdullah-git/BD_emo_translator.git",
+)
